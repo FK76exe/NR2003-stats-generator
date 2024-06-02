dad00c2e36247a3161cb0f868f4af0d5ab5077c4
FK76exe <xerevofaris76@gmail.com>
2024-06-02 14:38:49 -0400

    add race from website

--- src/report.py
+++ src/report.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from markupsafe import escape
+import file_scraper
 import sqlite3
 
 app = Flask(__name__)
@@ -10,9 +11,56 @@ DATABASE_NAME = "../db/nr-stats-gen.db"
 def hello_world():
     return "<p>Hello, World!</p>"
 
-@app.route("/<name>")
-def hello(name):
-    return f"Hello, {escape(name)}!"
+@app.route("/add-race", methods=['GET', 'POST'])
+def add_race():
+    # get all series and tracks
+    series = []
+    tracks = []
+    with sqlite3.connect(DATABASE_NAME) as con:
+        cursor = con.cursor()
+        cursor.execute(f"SELECT * FROM series ORDER BY name")
+        series = cursor.fetchall()
+        cursor.execute(f"SELECT * FROM tracks ORDER BY track_name")
+        tracks = cursor.fetchall()
+
+    # if GET -> present form
+    if request.method == 'GET':
+        return render_template('add_race.html', tracks=tracks, series=series)
+    # if POST -> add race data
+    race_series, race_year, race_track = request.form['series'], request.form['year'], request.form['tracks']
+    race_file = request.files['file']
+    processed = file_scraper.scrape_race_results(race_file.read().decode("windows-1252"))
+
+    drivers = [(row[3], ) for row in processed[1:]]
+
+    with sqlite3.connect(DATABASE_NAME) as con:
+        cursor = con.cursor()
+        cursor.execute(f"INSERT INTO races (series, year, race_file, track_id) VALUES ({int(race_series)}, {int(race_year)}, '{race_file.name}', {int(race_track)})")
+        
+        # create drivers if they don't exist
+        cursor.executemany(f"INSERT OR IGNORE INTO drivers (game_id) VALUES (?)", drivers)
+
+        # get id of race
+        cursor.execute(f"SELECT id FROM races WHERE race_file='{race_file.name}'")
+        race_id = cursor.fetchone()
+
+        # get id of drivers
+        cursor.execute(f"SELECT id, game_id FROM drivers")
+        driver_data = cursor.fetchall()
+        
+        driver_id_dict = {}
+        for driver in driver_data:
+            driver_id_dict[driver[1]] = driver[0]
+
+        # add driver data
+        for row in processed[1:]:
+            print(f"INSERT INTO race_records ({race_id[0]}, {row[0]}, {row[1]}, {row[2]}, {int(driver_id_dict[row[3]])}, {row[4]}, {row[5]}, {row[6]}, {row[7]}, '{row[8]}')")
+            cursor.execute(f"INSERT INTO race_records VALUES ({race_id[0]}, {row[0]}, {row[1]}, {row[2]}, {int(driver_id_dict[row[3]])}, '{row[4]}', {row[5]}, {row[6]}, {row[7]}, '{row[8]}')")
+
+        con.commit()
+
+    return "submitted"
+
 
 @app.route("/points/<series>/<year>")
 def show_series(series, year):
