6ce325a4abd5f0a7d027463d92459b29eb8b74fc
FK76exe <xerevofaris76@gmail.com>
2024-08-12 14:36:59 -0400

    revamped home page

--- src/src/main.py
+++ src/src/main.py
@@ -16,16 +16,4 @@ DATABASE_NAME = "../../db/nr-stats-gen.db"
 
 @app.route("/")
 def home():
-    season_year_dict = {}
-    with sqlite3.connect(DATABASE_NAME) as con:
-        cursor = con.cursor()
-        cursor.execute("SELECT DISTINCT series.name, season_num as year FROM races, series JOIN seasons ON races.season_id = seasons.id AND seasons.series_id = series.id ORDER BY series.name ASC, year DESC")
-        data = cursor.fetchall()
-        for row in data:
-            s,y = row
-            if s not in season_year_dict.keys():
-                season_year_dict[s] = [y]
-            else:
-                season_year_dict[s].append(y)
-
-    return render_template('home.html', season_year_dict=season_year_dict)
+    return render_template('home.html')
