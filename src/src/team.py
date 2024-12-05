171e12966c99dc63d3f8a696e8052e85e7c832ff
FK76exe <xerevofaris76@gmail.com>
2024-12-05 15:15:48 -0500

    view single team, rename team, delete team

--- src/src/team.py
+++ src/src/team.py
@@ -19,6 +19,23 @@ def view_teams():
             return redirect(url_for('team_page.view_teams'))
         
 
-@team_page.route("/<id>")
-def view_single_team():
-    pass
+@team_page.route("/<id>", methods=['GET', 'POST', 'DELETE'])
+def single_team(id):
+    with sqlite3.connect(DB_PATH) as con:
+        cursor = con.cursor()
+        if request.method == 'GET':
+            # retrieve team data
+            team_name = cursor.execute(f"SELECT name FROM teams WHERE id = {id}").fetchone()
+            if team_name:
+                return render_template("./teams/teams_single.html", team_name = team_name[0])
+            else:
+                return abort(404, "Team with this id does not exist.")
+        elif request.method == 'POST':
+            # update team name
+            cursor.execute(f"UPDATE teams SET name='{request.form['name']}' WHERE id = {id}")
+            return redirect(url_for('team_page.single_team', id=id))
+        else:
+            # delete team
+            cursor.execute(f"DELETE FROM teams WHERE id = {id}")
+            return redirect(url_for('team_page.view_teams'), 303) # HTTP 303 (See Other): redirect to new URL with GET
+    
